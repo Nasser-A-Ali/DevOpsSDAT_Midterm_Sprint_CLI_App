b20104379bd166094cc08f19c978b0d5010843da
sara woodford <sarawoodford6@gmail.com>
2025-02-15 14:30:58 -0330

    feat: album class implemented
    - attributes include artist, release year, number
    of songs, genre
    - getters and setters for all attributes
    - toString method to display album information

--- src/main/java/com/keyin/models/Album.java
+++ src/main/java/com/keyin/models/Album.java
@@ -1,4 +1,57 @@
 package com.keyin.models;
 
 public class Album {
+    private String artist;
+    private long releaseYear;
+    private int numberOfSongs;
+    private String genre;
+
+    public Album(String artist, long releaseYear, int numberOfSongs, String genre) {
+        this.artist = artist;
+        this.releaseYear = releaseYear;
+        this.numberOfSongs = numberOfSongs;
+        this.genre = genre;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public long getReleaseYear() {
+        return releaseYear;
+    }
+
+    public int getNumberOfSongs() {
+        return numberOfSongs;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setReleaseYear(long releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public void setNumberOfSongs(int numberOfSongs) {
+        this.numberOfSongs = numberOfSongs;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" +
+                "Artist= " + getArtist() +
+                ", Release Year= " + getReleaseYear() +
+                ", Number of Songs= " + getNumberOfSongs() +
+                ", Genre= " + getGenre() +
+                "}";
+    }
 }
