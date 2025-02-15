f60345392011652fcacf9bd89b3bde1304a4d8de
Adam Stevenson <160768532+Adam-S988@users.noreply.github.com>
2025-02-15 14:15:41 -0330

    Added in the Artist.java file

--- src/main/java/com/keyin/models/Artist.java
+++ src/main/java/com/keyin/models/Artist.java
@@ -1,4 +1,57 @@
 package com.keyin.models;
 
 public class Artist {
+    private String name;
+    private long debutYear;
+    private String genre;
+    private String country;
+
+    public Artist (String name, long debutYear, String genre, String country) {
+        this.name = name;
+        this.debutYear = debutYear;
+        this.genre = genre;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getDebutYear() {
+        return debutYear;
+    }
+
+    public void setDebutYear(long debutYear) {
+        this.debutYear = debutYear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "name='" + name + '\'' +
+                ", debutYear=" + debutYear +
+                ", genre='" + genre + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
